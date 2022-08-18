e12ded8b2fdc5b97c1e15f2b1e9b0e4f9d16165c
Sean Duffie <duffiesean@gmail.com>
2022-08-18 01:31:11 -0500

    Making pylint happy; Replace vid loop with enumerate;

--- camera.py
+++ camera.py
@@ -1,47 +1,51 @@
 #!/usr/bin/env python
+"""
+Docstring for camera.py.
+"""
 
-from picamera import PiCamera
 from datetime import datetime
 from time import sleep
 import os
+import glob
+from picamera import PiCamera
 
 import cv2
-import numpy as np
-import glob
 
-path = os.getcwd()
+SDIR = os.getcwd()
 
 with PiCamera() as camera:
     # Acquire current time
-    now = datetime.now()
-    hour = now.strftime("%H")
-    imgname = now.strftime("{0}/autocaps/%Y-%m-%d_%H:%M:%S.jpg".format(path))
+    NOW = datetime.now()
+    H = NOW.strftime("%H")
+    IMG_NAME = NOW.strftime("{0}/autocaps/%Y-%m-%d_%H:%M:%S.jpg".format(SDIR))
 
     # If during inactive hours, do nothing
-    if (int(hour) >= 7 or int(hour) == 0):
+    if (int(H) >= 7 or int(H) == 0):
     # if (False):
         # Start camera and wait to focus
         camera.start_preview()
         sleep(5)
         # Capture image and stop
-        camera.capture(imgname)
+        camera.capture(IMG_NAME)
         camera.stop_preview()
 
         # Read in current directory of images
-        img_array = []
-        for filename in glob.glob('{0}/autocaps/*.jpg'.format(path)):
+        IMG_ARR = []
+        IMG_SIZE = (0, 0)
+        for filename in glob.glob('{0}/autocaps/*.jpg'.format(SDIR)):
             img = cv2.imread(filename)
             height, width, layers = img.shape
-            size = (width,height)                   # image size
-            img_array.append(img)
+            IMG_SIZE = (width, height)                   # image size
+            IMG_ARR.append(img)
 
         # Compile image array into a video
-        vidname = '{0}/time-lapse.mp4'.format(path) # video name
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V')    # video format
-        fps = 60                                    # video fps
-        out = cv2.VideoWriter(vidname, fourcc, fps, size)
-        for i in range(len(img_array)):
-            out.write(img_array[i]) # writes out each frame to the video file
-        out.release()
-
-    pass
+        VID_NAME = '{0}/time-lapse.mp4'.format(SDIR) # video name
+        FOURCC = cv2.VideoWriter_fourcc(*'MP4V')    # video format
+        FPS = 60                                    # video fps
+        OUT = cv2.VideoWriter(VID_NAME, FOURCC, FPS, IMG_SIZE)
+        # for i in range(len(IMG_ARR)):
+        #     OUT.write(IMG_ARR[i]) # writes out each frame to the video file
+        OUT.write(list(enumerate(IMG_ARR))) # writes out each frame to the video file
+        OUT.release()
+    else:
+        print("Inactive hours")
