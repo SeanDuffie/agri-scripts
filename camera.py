e3b71c03f26aa0f7b465aae0ffd2ca36e17ec7f7
Sean Duffie <duffiesean@gmail.com>
2022-08-22 19:49:50 -0500

    Skeleton code for additional features

--- camera.py
+++ camera.py
@@ -22,20 +22,37 @@ with PiCamera() as camera:
     # If during inactive hours, do nothing
     if (int(H) >= 7 or int(H) == 0):
     # if (False):
-        # Start camera and wait to focus
+        ### Start Acquire Image ###
         camera.start_preview()
-        sleep(5)
+        sleep(5) # wait for camera to focus
         # Capture image and stop
         camera.capture(IMG_NAME)
         camera.stop_preview()
+        #### End Acquire Image ####
 
+
+        ### TODO: Start Acquire Sensor Measurements ###
+        
+        #### End Acquire Sensor Measurements ####
+
+
+        ### Post Processing Video Compilation ###
         # Read in current directory of images
         IMG_ARR = []
         IMG_SIZE = (0, 0)
         for filename in glob.glob('{0}/autocaps/*.jpg'.format(SDIR)):
+            # Read in Raw image
             img = cv2.imread(filename)
             height, width, layers = img.shape
             IMG_SIZE = (width, height)                   # image size
+
+            # TODO: Add Timestamp
+
+
+            # TODO: Append Sensor data somehow
+
+
+            # Add image to array
             IMG_ARR.append(img)
 
         # Compile image array into a video
@@ -43,9 +60,8 @@ with PiCamera() as camera:
         FOURCC = cv2.VideoWriter_fourcc(*'MP4V')    # video format
         FPS = 60                                    # video fps
         OUT = cv2.VideoWriter(VID_NAME, FOURCC, FPS, IMG_SIZE)
-        # for i in range(len(IMG_ARR)):
-        #     OUT.write(IMG_ARR[i]) # writes out each frame to the video file
         OUT.write(list(enumerate(IMG_ARR))) # writes out each frame to the video file
         OUT.release()
+        #### End Post Processing Video Compilation ####
     else:
         print("Inactive hours")
