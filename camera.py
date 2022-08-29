953a812e7552b1e45dc05aeb042c901edb4d8f6e
Sean Duffie <duffiesean@gmail.com>
2022-08-29 00:00:04 -0500

    Added Pyserial; Data is now read in from the arduino!

--- camera.py
+++ camera.py
@@ -8,14 +8,20 @@ from time import sleep
 import os
 import cv2
 # import numpy
+import serial
 from serial.tools import list_ports
 # from picamera import PiCamera
+
+# Versions
+# print("Package Versions for Diagnostics: ")
 
 # Acquire initial data
 RTDIR = os.getcwd()
 IMGDIR = RTDIR + "/autocaps/"
 print("Root: ", RTDIR)
 print("Images: ", IMGDIR)
+print("Output: ./time-lapse.mp4")
+print()
 
 VID_NAME = '{0}/time-lapse.mp4'.format(RTDIR)   # video name
 FOURCC = cv2.VideoWriter_fourcc(*'mp4v')        # video format
@@ -24,33 +30,62 @@ IMG_SIZE = (1920, 1080)
 
 NOW = datetime.now()
 H = NOW.strftime("%H")
+print("Current time: ", NOW)
+print()
+
 
 # If during inactive hours, do nothing
 if (int(H) >= 7 or int(H) == 0):
 # if (True):
     # ### Start Acquire Image ###
     # with PiCamera() as camera:
+    #     print("Starting Camera...")
     #     camera.start_preview()
     #     sleep(5) # wait for camera to focus
-    #     # ## Capture image and stop
+    #     ## Capture image and stop
     #     IMG_NAME = NOW.strftime("{0}/autocaps/%Y-%m-%d_%Hh%Mm%Ss.jpg".format(RTDIR))
     #     camera.capture(IMG_NAME)
     #     camera.stop_preview()
+    #     print("Picture Acquired!")
+    #     print()
     # #### End Acquire Image ####
 
 
     ### TODO: Start Acquire Sensor Measurements ###
     # Identifying ports...
-    port = list(list_ports.comports())
-    for p in port:
-        print(p.device)
+    print("Identifying current ports... ")
+    PORT_LIST = list(list_ports.comports())
+    for p in PORT_LIST:
+        print("  - ", p.device)
+    print()
 
-    # Send the Request for readings
+    # Setting up Serial connection
+    ser = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+    # Send out requests until a response is received
+    while ser.in_waiting == 0:
+        print("Waiting for sensors...")
+        ser.write('1'.encode('utf-8'))
+        sleep(1)
 
     # Receive the Response from the sensors
+    FEEDBACK = ser.readline()
+    try:
+        print(FEEDBACK.decode("Ascii"))
+        print()
+    except:
+        print("Something went wrong with decoding!")
+        pass
 
     # Process and Store the new data
-    
+
     #### End Acquire Sensor Measurements ####
 
 
