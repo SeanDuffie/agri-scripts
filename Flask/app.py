4859bab756fe1c9843942b60b950df53b84fb2a3
SeanDuffie <duffiesean@gmail.com>
2024-02-13 16:31:59 -0500

    Added new framework for future webpage with app.py Resorted imports server.py Added placeholders for index, style, and script

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, send_from_directory
+""" @file app.py
+    @author Sean Duffie
+    @brief Flask Framework for webserver to be run on RPi
+    
+    This website will allow the user to view data collected from the Sensors and Camera
+"""
+from flask import Flask, render_template, send_from_directory, url_for, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return send_from_directory("./scripts/", "index.js")
+def welcome():
+    return render_template("index.html")
 
 @app.route("/roll")
 def roll():
